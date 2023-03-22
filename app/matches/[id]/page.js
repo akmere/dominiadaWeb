9b65f431fc5447cbeb030e0f93955e7d111486ad
akmere <rafkam0@gmail.com>
2023-03-23 00:28:42 +0100

    fixed recordings api

--- app/matches/[id]/page.js
+++ app/matches/[id]/page.js
@@ -8,7 +8,8 @@ export default async function Match({ params, searchParams }) {
   let match = await prisma.$queryRaw`SELECT *, TO_CHAR(TO_TIMESTAMP(date::VARCHAR(25), 'YYYYMMDDHH24MISS') AT TIME ZONE 'Europe/Warsaw', 'DD/MM/YYYY, HH24:MI:SS') AS ddate FROM Matches WHERE Pk=${matchId}`;
   let goals = await prisma.$queryRaw`SELECT * FROM Goals WHERE MatchId=${matchId}`;
   let appearances = await prisma.$queryRaw`SELECT * FROM Appearances WHERE MatchId=${matchId}`;
-  let recordingAvailable = await (await fetch(`http://localhost:3000/api/recordings/${matchId}`)).json();
+  let recordingResponse = await fetch(`http://localhost:3000/api/recordings/${matchId}`);
+  let recordingAvailable = recordingResponse.status === 200 ? true : false;
   [match, goals, appearances, recordingAvailable] = await Promise.all([match, goals, appearances, recordingAvailable]);
 
   return (
@@ -17,7 +18,7 @@ export default async function Match({ params, searchParams }) {
         <iframe src={`/falafel/costam.html?id=${matchId}`} width={'1500px'} height={'850px'}></iframe>
       </div>
       <div className="card" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '600px' }}>
-        <a className= {recordingAvailable ? `` : `disabled-link`} href={`/api/getRecording/${match[0].pk}`}><i style={{color: recordingAvailable ? 'lightgreen' : '#FF7F7F'}} className="bi bi-file-earmark-arrow-down"></i></a>
+        <a className= {recordingAvailable ? `` : `disabled-link`} href={`/api/recordings/${match[0].pk}`}><i style={{color: recordingAvailable ? 'lightgreen' : '#FF7F7F'}} className="bi bi-file-earmark-arrow-down"></i></a>
         <h2>{match[0].ddate}</h2>
         <h3>{match[0].stadium}</h3>
         <h3>{match[0].format} vs. {match[0].format}</h3>        
